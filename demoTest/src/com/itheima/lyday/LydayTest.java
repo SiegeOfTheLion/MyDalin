81a75ce1265bf90297ca3a80f553fccda2b66f0a
git123 <1065767471@qq.com>
2018-04-03 09:00:15 +0800

    测试类

---
+++ demoTest/src/com/itheima/lyday/LydayTest.java
@@ -0,0 +1,14 @@
+package com.itheima.lyday;  
+/**  
+ * ClassName:LydayTest <br/>  
+ * Function:  <br/>  
+ * Date:     2018年4月3日 上午8:50:17 <br/>       
+ */
+public class LydayTest {
+    public static void main(String[] args) {
+        System.out.println("你好,lyday!");
+        System.out.println("             ");
+        MyDear.say();
+    }
+}
+  
---
+++ demoTest/src/com/itheima/lyday/MyDear.java
@@ -0,0 +1,12 @@
+package com.itheima.lyday;  
+/**  
+ * ClassName:MyDear <br/>  
+ * Function:  <br/>  
+ * Date:     2018年4月3日 上午8:51:53 <br/>       
+ */
+public class MyDear {
+    public static void say(){
+        System.out.println("I Love You lyday!!!");
+    }
+}
+  
